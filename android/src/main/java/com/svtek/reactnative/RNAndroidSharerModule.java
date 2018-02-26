adec40554b87ba788ad419f72f7d1eb3d31ac6de
Ahmet Ardal <ardalahmet@gmail.com>
2018-02-26 21:53:24 +0300

    Implement shareViaSms method

--- android/src/main/java/com/svtek/reactnative/RNAndroidSharerModule.java
+++ android/src/main/java/com/svtek/reactnative/RNAndroidSharerModule.java
@@ -1,6 +1,10 @@
 
 package com.svtek.reactnative;
 
+import android.content.Intent;
+import android.net.Uri;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -18,5 +22,19 @@ public class RNAndroidSharerModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNAndroidSharer";
+  }
+
+  @ReactMethod
+  public void shareViaSms(String filePath, final Promise promise) {
+    try {
+      Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+      smsIntent.setType("vnd.android-dir/mms-sms");
+      smsIntent.putExtra("sms_body","https://www.leoapp.com");
+      smsIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(filePath));
+      this.reactContext.startActivity(smsIntent);
+      promise.resolve();
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
   }
 }
